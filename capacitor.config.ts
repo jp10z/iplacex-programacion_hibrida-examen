c207b4124c2752e4ded2f8aa83bff6827ef002da
José P. Ramos <jpramos2910@gmail.com>
2024-11-24 12:01:03 -0300

    Se agrega el código inicial

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'iplacex.programacion2.examensrc',
+  appName: 'iplacex-programacion2-examensrc',
+  webDir: 'www'
+};
+
+export default config;
