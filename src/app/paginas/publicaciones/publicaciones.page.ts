6ba398dc372971bae2d7ca393ae1ff0a6a427e63
José P. Ramos <jpramos2910@gmail.com>
2024-11-24 12:13:35 -0300

    Se agrega floating-icon mostrado en la página de publicaciones

--- src/app/paginas/publicaciones/publicaciones.page.ts
+++ src/app/paginas/publicaciones/publicaciones.page.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonFabButton, IonFab, IonIcon } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
 
 @Component({
   selector: 'app-publicaciones',
   templateUrl: './publicaciones.page.html',
   styleUrls: ['./publicaciones.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonFab, IonFabButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterModule]
 })
 export class PublicacionesPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({
+      add
+    });
+  }
 
   ngOnInit() {
   }
