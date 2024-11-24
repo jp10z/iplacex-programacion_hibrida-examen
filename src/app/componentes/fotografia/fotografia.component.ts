f596bc276904b207746f276c8229aa2233cf4a32
José P. Ramos <jpramos2910@gmail.com>
2024-11-24 13:56:21 -0300

    Se agrega la captura de fotos

--- src/app/componentes/fotografia/fotografia.component.ts
+++ src/app/componentes/fotografia/fotografia.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { IonButton, IonIcon, IonImg, IonCol, IonRow, IonGrid } from "@ionic/angular/standalone";
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { IonButton, IonIcon, IonImg } from "@ionic/angular/standalone";
 import { addIcons } from 'ionicons';
 import { cameraOutline } from 'ionicons/icons';
 
@@ -8,12 +9,14 @@ import { cameraOutline } from 'ionicons/icons';
   templateUrl: './fotografia.component.html',
   styleUrls: ['./fotografia.component.scss'],
   standalone: true,
-  imports: [IonGrid, IonRow, IonCol, IonImg, 
+  imports: [IonImg, 
     IonButton,
     IonIcon
   ]
 })
 export class FotografiaComponent  implements OnInit {
+
+  fotografiaSRC: string = "assets/imagen-placeholder.svg";
 
   constructor() {
     addIcons({
@@ -22,5 +25,18 @@ export class FotografiaComponent  implements OnInit {
   }
 
   ngOnInit() {}
+
+  async capturarFotografia() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64
+    });
+
+    if (image.base64String === undefined) {
+      return;
+    }
+    this.fotografiaSRC = `data:image/jpeg;base64, ${image.base64String}`;
+  }
 
 }
